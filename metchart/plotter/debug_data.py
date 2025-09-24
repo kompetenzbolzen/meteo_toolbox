03f1fa57aa8a2634660c0bab13995226787ced1a
Jonas Gunz <himself@jonasgunz.de>
2025-09-24 17:06:48 +0200

    [plotter/debug_data] update Debug plotter to new format

--- metchart/plotter/debug_data.py
+++ metchart/plotter/debug_data.py
@@ -1,4 +1,15 @@
-def run(data, **kwargs):
-    print(data)
+from . import Plotter
+from ..aggregator import DataView, Variable
 
-    return []
+class Debug (Plotter):
+    def _init(self):
+        pass
+
+    def _load_config(self):
+        pass
+
+    def _report_needed_variables(self) -> list[Variable]:
+        return []
+
+    def _plot(self, view: DataView, filename_prefix: str):
+        print(view.get())
